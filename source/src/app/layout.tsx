c338898344f0740d832fa379e04333bff96ffd65
ivan17039 <ivanbarada1703@gmail.com>
2024-12-06 22:34:10 +0100

    Added footer

--- source/src/app/layout.tsx
+++ source/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Navigation } from "@/components/navigation";
+import { Footer } from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Navigation/>
         {children}
+        <Footer/>
       </body>
     </html>
   );

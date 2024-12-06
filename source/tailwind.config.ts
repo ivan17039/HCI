38b0e22d6c133963648c9b78ed90b21efa4f4611
ivan17039 <ivanbarada1703@gmail.com>
2024-12-06 22:44:50 +0100

    Other changes in tailwind and package.json

--- source/tailwind.config.ts
+++ source/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -11,12 +12,32 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: '#40E0D0',  // Tirkizna boja
-        accent: '#FFD700',   // Zlatna boja
+        primary: "rgb(96 165 250)", // Tirkizna boja
+        accent: "#FFB84D",  // Zlatna boja
+        logoblue: "#282A9B", // Tamno plava boja loga
+      },
+      screens: {
+        customLg: '1150px', // Dodavanje prilagođenog breakpointa
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".text-red-border": {
+          WebkitTextStroke: "2px #D2042D", // Crveni obrub debljine 2px
+          color: "white", // Boja slova
+          textShadow: "1px 2px 10px #D2042D, 1.5px 1.5px 1px #D2042D", // Crvena sjena s obje strane
+        },
+        ".text-shadow-turquoise": {
+          textShadow: "2px 2px 4px #20B2AA", // Tirkizna sjena
+        },
+        ".text-stroke-black": {
+          WebkitTextStroke: "0.5px black", // Obrub u crnoj boji
+          color: "white", // Osnovna boja slova
+        },
+      });
+    }),
+  ],
 };
-
 export default config;

a65c71331c7afaf793138b721348a87871339033
ivan17039 <ivanbarada1703@gmail.com>
2025-02-14 19:32:38 +0100

    Updated login page

--- source/src/app/login/page.tsx
+++ source/src/app/login/page.tsx
@@ -1,9 +1,22 @@
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/app/login/components/login-form";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | Your App Name",
+  description: "Login to your account",
+};
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center h-screen text-black">
-      <LoginForm />
+    <div className="flex justify-center items-center min-h-screen bg-secondary px-4 sm:px-6 lg:px-8 text-gray-700">
+      <div className="w-full max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Log in to your account
+        </h2>
+        <div className="mt-8">
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 }
